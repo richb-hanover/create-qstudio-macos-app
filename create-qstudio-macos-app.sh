6c0be9b36b7d0db0acf2270fbc473409d957fe43
Rich Brown <richb.hanover@gmail.com>
2024-10-10 18:33:27 -0400

    Include both prqlc x86 and arm64 binaries

--- create-qstudio-macos-app.sh
+++ create-qstudio-macos-app.sh
@@ -4,21 +4,30 @@
 
 # Usage: sh ./create-qstudio-macos-app.sh ./qstudio.jar
 #
-# 
+# For example:
+#  sh ./create-qstudio-macos-app.sh /Applications/qstudio.jar
+
 # First argument is the .jar file itself
 # Output is a clickable app, saved within the repo
+# The app bundle is .gitignored, so the repo isn't too big
 
-# Remove the previous version
+# Remove the previous qStudio.app and rebuild anew
 rm -rf qStudio.app
 
 # Copy the qStudio .jar file into the bundle
 mkdir -p qStudio.app/Contents/Resources
 cp "$1" qStudio.app/Contents/Resources/qstudio.jar
-cp /usr/local/bin/prqlc qStudio.app/Contents/Resources/prqlc
+
+# Get both the x86 and arm64 versions of `prqlc`
+# Should download the x86 version instead of sponging off my local copy
+# prqlc is about 5 megabytes, so it's no big deal to import both
+mkdir -p qStudio.app/Contents/Resources/x86/
+mkdir -p qStudio.app/Contents/Resources/arm64/
+cp /usr/local/bin/prqlc qStudio.app/Contents/Resources/x86/prqlc
+wget -O qStudio.app/Contents/Resources/arm64/prqlc \
+	https://github.com/PRQL/prql/releases/download/0.13.0/prqlc-0.13.0-aarch64-apple-darwin.tar.gz
  
 # Copy the "script to execute" into the bundle
-# Maybe see: https://github.com/timeseries/qstudio/issues/93
-
 mkdir -p qStudio.app/Contents/MacOS
 cp run-qstudio.sh qStudio.app/Contents/MacOS/run-qstudio.sh
 
--- run-qstudio.sh
+++ run-qstudio.sh
@@ -1,4 +1,15 @@
 #! /bin/sh
 
-export PATH="$(dirname "$0")/../Resources:$PATH"
+# Set the path to the proper `prqlc` binary:
+ARCH=$(uname -m)
+
+if [ "$ARCH" = "x86_64" ]; then
+    export PATH="$(dirname "$0")/../Resources/x86:$PATH"
+elif [ "$ARCH" = "arm64" ]; then
+    export PATH="$(dirname "$0")/../Resources/arm64:$PATH"
+else
+    echo "Unknown architecture: $ARCH"
+fi
+
+# Launch the java app
 java -jar "$(dirname "$0")/../Resources/qstudio.jar"
