df240cead2480509ac78d07bfe8bae52aec72048
Rich Brown <richb.hanover@gmail.com>
2024-10-11 19:12:42 -0400

    Download qStudio.jar; Download x86 and arm64 versions of prqlc Fixup version number shown in Finder

--- create-qstudio-macos-app.sh
+++ create-qstudio-macos-app.sh
@@ -1,44 +1,55 @@
 #! /bin/bash
 
-# Create a clickable macOS app bundle from a java .jar file
+# Create a clickable macOS app bundle from the downloaded qStudio
 
-# Usage: sh ./create-qstudio-macos-app.sh ./qstudio.jar
+# Usage: sh ./create-qstudio-macos-app.sh 
 #
 # For example:
-#  sh ./create-qstudio-macos-app.sh /Applications/qstudio.jar
+#  sh ./create-qstudio-macos-app.sh 
 
-# First argument is the .jar file itself
 # Output is a clickable app, saved within the repo
 # The app bundle is .gitignored, so the repo isn't too big
 
-# Remove the previous qStudio.app and rebuild anew
+QSTUDIO_VERSION="3.10"
+PRQLC_VERSION="0.13.1"
+QSTUDIO_DOWNLOAD="https://www.timestored.com/qstudio/files/qstudio.jar"
+
+# Remove the previous qStudio.app bundle and rebuild anew
 rm -rf qStudio.app
 
-# Copy the qStudio .jar file into the bundle
+# Download the qStudio .jar file from the qStudio server
+echo "Downloading qStudio.jar"
 mkdir -p qStudio.app/Contents/Resources
-cp "$1" qStudio.app/Contents/Resources/qstudio.jar
+wget  -q -O qStudio.app/Contents/Resources/qstudio.jar "$QSTUDIO_DOWNLOAD"
 
 # Get both the x86 and arm64 versions of `prqlc`
-# Should download the x86 version instead of sponging off my local copy
-# prqlc is about 5 megabytes, so it's no big deal to import both
+# prqlc is about 18 megabytes, so it's not too big a deal to import both
 mkdir -p qStudio.app/Contents/Resources/x86/
 mkdir -p qStudio.app/Contents/Resources/arm64/
-cp /usr/local/bin/prqlc qStudio.app/Contents/Resources/x86/prqlc
-wget -O qStudio.app/Contents/Resources/arm64/prqlc \
-	https://github.com/PRQL/prql/releases/download/0.13.0/prqlc-0.13.0-aarch64-apple-darwin.tar.gz
- 
-# Copy the "script to execute" into the bundle
+
+echo "Downloading prqlc x86 binary"
+wget -O - -q "https://github.com/PRQL/prql/releases/download/$PRQLC_VERSION/prqlc-$PRQLC_VERSION-x86_64-apple-darwin.tar.gz" | \
+	tar -xz -C qStudio.app/Contents/Resources/x86/   prqlc
+
+echo "Downloading prql arm64 binary"
+wget -O - -q "https://github.com/PRQL/prql/releases/download/$PRQLC_VERSION/prqlc-$PRQLC_VERSION-aarch64-apple-darwin.tar.gz" | \
+	tar -xz -C qStudio.app/Contents/Resources/arm64/ prqlc
+
+# Copy the "script to execute" into the bundle & make it executable
+echo "Adding startup script"
 mkdir -p qStudio.app/Contents/MacOS
 cp run-qstudio.sh qStudio.app/Contents/MacOS/run-qstudio.sh
-
-# Copy in the Info.plist
-cp Info.plist qStudio.app/Contents
-
-# Copy in the .icns file
-cp qStudio.icns qStudio.app/Contents/Resources
-
-# Make the bundle executable
 chmod +x qStudio.app/Contents/MacOS/run-qstudio.sh
 
-# Make everything executable
-chmod -R 755 qStudio.app
+# Copy in the Info.plist & update short version string
+echo "Updating Info.plist"
+cp Info.plist qStudio.app/Contents
+NEW_VERSION="qStudio $QSTUDIO_VERSION - prqlc $PRQLC_VERSION"
+plist_path="qStudio.app/Contents/Info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $NEW_VERSION" "$plist_path"
+
+# Copy in the .icns file
+echo "Adding .icns"
+cp qStudio.icns qStudio.app/Contents/Resources
+
+echo "Done"
