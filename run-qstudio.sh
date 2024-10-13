8e797ca2fcf780d3eb7c1cc016caa162c8638863
Rich Brown <richb.hanover@gmail.com>
2024-10-13 10:36:21 -0400

    Test for ability to run java

--- run-qstudio.sh
+++ run-qstudio.sh
@@ -11,5 +11,12 @@ else
     echo "Unknown architecture: $ARCH"
 fi
 
-# Launch the java app
-java -jar "$(dirname "$0")/../Resources/qstudio.jar"
+# Check if Java is functional (detecting the version)
+if java -version &> /dev/null; then
+    # Launch the java app
+    java -jar "$(dirname "$0")/../Resources/qstudio.jar"
+else
+    echo "Java is not installed."
+    stderr_output=$(java -version 2>&1 1>/dev/null)
+    osascript -e 'display alert "Java not installed" message "$stderr_output" as critical buttons {"OK"} default button "OK"'
+fi
