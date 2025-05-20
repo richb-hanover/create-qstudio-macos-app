b4d450d1388b9832e04aec709b44518dc073fa1a
Rich Brown <richb.hanover@gmail.com>
2025-05-20 07:39:12 -0400

    QStudio version 4.08; prqlc version 0.13.4; Add error checking to abort if wget can't get the file;

--- create-qstudio-macos-app.sh
+++ create-qstudio-macos-app.sh
@@ -14,8 +14,8 @@
 # Output is a clickable app, saved within the repo
 # .gitignore the app bundle and its .zip, so the repo isn't too big
 
-PRQLC_VERSION="0.13.2"
-QSTUDIO_PRODUCTION_VERSION="4.03"
+PRQLC_VERSION="0.13.4"
+QSTUDIO_PRODUCTION_VERSION="4.08"
 QSTUDIO_BETA_VERSION="3.86"
 
 QSTUDIO_DOWNLOAD="https://github.com/timestored/qstudio/releases/download/$QSTUDIO_PRODUCTION_VERSION/qstudio.jar"
@@ -39,16 +39,19 @@ echo "*** Update QStudio and prqlc version numbers within the script before runn
 if [ "$1" == "" ]; then
 	QSTUDIO_URL="$QSTUDIO_DOWNLOAD"
 	QSTUDIO_VERSION="$QSTUDIO_PRODUCTION_VERSION"
-	echo "*** Building production release: Version $QSTUDIO_PRODUCTION_VERSION"
+	echo "*** Building production release:"
 elif [ "$1" == "beta" ]; then
 	QSTUDIO_URL="$QSTUDIO_BETA_DOWNLOAD"
 	QSTUDIO_VERSION="$QSTUDIO_BETA_VERSION"
-	echo "*** Building beta release: Version $QSTUDIO_BETA_VERSION"
+	echo "*** Building beta release:"
 else
 	echo "*** Unknown option '$1' ***"
 	exit
 fi
-echo ""
+
+echo "   QStudio Version: $QSTUDIO_VERSION"
+echo "   prqlc Version:   $PRQLC_VERSION"
+
 
 # Remove the previous QStudio.app bundle and rebuild anew
 rm -rf QStudio.app
@@ -56,20 +59,27 @@ rm -rf QStudio.app
 # Download the QStudio .jar file from the QStudio server
 echo "Downloading QStudio.jar"
 mkdir -p QStudio.app/Contents/Resources
-wget  -q -O QStudio.app/Contents/Resources/qstudio.jar "$QSTUDIO_URL"
+wget  -q -O QStudio.app/Contents/Resources/qstudio.jar "$QSTUDIO_URL" \
+   ||  { echo "   *** QStudio download failed ***"; exit 1; }
 
 # Get both the x86 and arm64 versions of `prqlc`
 # prqlc is about 18 megabytes, so it's not too big a deal to import both
 
 echo "Downloading prqlc binary x86"
 mkdir -p QStudio.app/Contents/Resources/x86/
+
+set -o pipefail # return a failure of any step in a pipe as the result of the pipe
 wget -O - -q "$PRQLC_RELEASES/download/$PRQLC_VERSION/prqlc-$PRQLC_VERSION-x86_64-apple-darwin.tar.gz" | \
-	tar -xz -f - -C QStudio.app/Contents/Resources/x86/ ./prqlc 
+	tar -xz -f - -C QStudio.app/Contents/Resources/x86/ ./prqlc \
+  || { echo "   *** prqlc x86 download failed ***"; exit 1; } 
 
 echo "Downloading prqlc binary arm64"
 mkdir -p QStudio.app/Contents/Resources/arm64/
+
+set -o pipefail # return a failure of any step in a pipe as the result of the pipe
 wget -O - -q "$PRQLC_RELEASES/download/$PRQLC_VERSION/prqlc-$PRQLC_VERSION-aarch64-apple-darwin.tar.gz" | \
-	tar -xz -f - -C QStudio.app/Contents/Resources/arm64/ ./prqlc 
+	tar -xz -f - -C QStudio.app/Contents/Resources/arm64/ ./prqlc \
+  || { echo "   *** prqlc arm download failed ***"; exit 1; }
 
 # Copy the script that launches the .jar file into the bundle & make it executable
 echo "Adding startup script"
